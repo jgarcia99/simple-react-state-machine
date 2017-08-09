a44e80b3727b71ac131aad0d524c2afe42fe178a
Josuel C Garcia <garciajosuel@yahoo.com>
2017-08-09 07:02:46 -0400

    initial setup

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "babel-polyfill"
+import React, { Component } from 'react'
+import ReactDom from 'react-dom'
+
+require('./style.css')
+
+class HelloWorld extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Hello World.</h1>
+                
+            </div>
+        )
+    }
+}
+const content = document.getElementById('app')
+ReactDom.render(<HelloWorld />, content)
